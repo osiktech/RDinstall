c38cba7a9e2bf5750cfb5b8f3b9cd94670972fcf
Stefan Tollkühn <s.tollkuehn@bnvb-ingenieure.de>
2023-09-15 02:57:45 +0200

    Initial commit of script to install Rustdesk client on MS Windows

---
+++ RustdeskInstall.ps1
@@ -0,0 +1,320 @@
+<#
+  .SYNOPSIS
+  Install and configure Rustdesk Client
+
+  .DESCRIPTION
+  Deployment script to deploy the latest Rustdesk (https://rustdesk.com/) client on a windows computer. Use described parameters to configure the client.
+
+  .PARAMETER rdServer
+  The IP address or FQDN of the RustDesk ID Server and Relay Server
+
+  .PARAMETER rdKey
+  Specifies the key of ID/Relay Server
+
+  .PARAMETER pwLength
+  Specifies the length of the password to connect to the RustDesk client
+
+  .PARAMETER enableAudio
+  EnableAudio in RustDesk client.
+
+  .EXAMPLE
+  PS> .\RustdeskInstall.ps1 -rdServer "somehost.example.tld" -rdKey "KeyFromServer="
+    Install RustDesk Client by defining a different ID/Relay server and corresponding key
+
+  .EXAMPLE
+  PS> .\RustdeskInstall.ps1 -rdServer "somehost.example.tld" -rdKey "KeyFromServer=" -pwLength 24
+    Optionally define length for client password
+
+  .EXAMPLE
+  PS> .\RustdeskInstall.ps1 -rdServer "somehost.example.tld" -rdKey "KeyFromServer=" -enableAudio 0
+    Optionally disable audio
+
+  .EXAMPLE
+  PS> .\RustdeskInstall.ps1 -toNextcloudPassword 1 -ncBaseUrl "https://some.nextcloud.url/index.php/apps/passwords" -ncUsername "user.name" -ncToken "12345-abcde-67890" -ncFolder "NextcloudPasswordFolderUUID"
+#>
+
+param(
+  [string]$rdServer = "rs-ny.rustdesk.com",
+  [string]$rdKey = $null,
+  [int]$pwLength = 8,
+  [bool]$enableAudio = $True,
+  [bool]$toNextcloudPassword = $False,
+  [string]$ncBaseUrl,
+  [string]$ncUsername,
+  [string]$ncToken,
+  [string]$ncFolder
+)
+
+if ($rdServer -ne "rs-ny.rustdesk.com") {
+  if (!($rdKey)) {
+    Write-Host("Required parameter '-rdKey' was not set! Exiting!")
+    exit 1
+  }
+}
+
+if ($toNextcloudPassword) {
+  if (!($ncBaseUrl)) {
+    Write-Host("Required parameter '-ncBaseUrl' was not set! Exiting!")
+    exit 1
+  }
+  if (!($ncUsername)) {
+    Write-Host("Required parameter '-ncUsername' was not set! Exiting!")
+    exit 1
+  }
+  if (!($ncToken)) {
+    Write-Host("Required parameter '-ncToken' was not set! Exiting!")
+    exit 1
+  }
+  if (!($ncFolder)) {
+    Write-Host("Require parameter '-ncFolder' was not set! Exiting!")
+    exit 1
+  }
+}
+
+$ErrorActionPreference = 'silentlycontinue'
+#Run as administrator and stays in the current directory
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+  if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+    Start-Process PowerShell -Verb RunAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+    exit;
+  }
+}
+
+$rustdeskURL = 'https://github.com/rustdesk/rustdesk/releases/latest'
+$rustdeskReg = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\RustDesk\'
+
+function ExecuteCommand ($commandPath, $commandArguments) {
+  Write-Output("ExecuteCommand")
+  $pinfo = New-Object System.Diagnostics.ProcessStartInfo
+  $pinfo.FileName = $commandPath
+  $pinfo.RedirectStandardError = $true
+  $pinfo.RedirectStandardOutput = $true
+  $pinfo.UseShellExecute = $false
+  $pinfo.Arguments = $commandArguments
+  $p = New-Object System.Diagnostics.Process
+  $p.StartInfo = $pinfo
+  $p.Start() | Out-Null
+  $p.WaitForExit()
+  [pscustomobject]@{
+    stdout = $p.StandardOutput.ReadToEnd()
+    stderr = $p.StandardError.ReadToEnd()
+    ExitCode = $p.ExitCode
+  }
+}
+
+function PreqRustdeskUpstreamVersion([string]$rustdeskURL) {
+  Write-Output("PreqRustdeskUpstreamVersion")
+  #Get latest upstream version number
+  $upstream_rustdesk_version = [System.Net.WebRequest]::Create($rustdeskURL).GetResponse().ResponseUri.OriginalString.split('/')[-1].Trim('v')
+  return $upstream_rustdesk_version
+}
+
+function PreqRustdeskInstalledVersion([string]$rustdeskReg) {
+  Write-Output("PreqRustdeskInstalledVersion")
+  #Check if Rustdesk is already installed and get version number
+  if (Test-Path $rustdeskReg) {
+    $installed_rustdesk_version = ((Get-ItemProperty $rustdeskReg).Version)
+    return $installed_rustdesk_version
+  } else {
+    return 0
+  }
+}
+
+function Prerequisites([string]$VersionInstalled, [string]$VersionUpstream) {
+  if (!(Test-Path $env:Temp)) {
+    New-Item -ItemType Directory -Force -Path $env:Temp > null
+  }
+
+  if (!([System.Version]$VersionUpstream -gt [System.Version]$VersionInstalled)) {
+    Write-Output("Rustdesk version " + $VersionUpstream + " is already installed!")
+    exit
+  }
+}
+
+function DownloadRustdesk([string]$version) {
+  Invoke-WebRequest "https://github.com/rustdesk/rustdesk/releases/download/$version/rustdesk-$version-x86_64.exe" -Outfile "$env:Temp\rustdesk.exe"
+}
+
+function InstallRustdesk {
+  .$env:Temp\rustdesk.exe --silent-install
+  Start-Sleep 20
+  # Workaround: --silent-install does not quit process
+  Stop-Process -Name Rustdesk -Force > null
+}
+
+function StartRustdesk([string]$serviceName) {
+  Start-Service $serviceName
+}
+
+function StopRustdesk([string]$serviceName) {
+  $serviceState = Get-Service -Name $serviceName -ErrorAction SilentlyContinue
+
+  if ($serviceState -eq $null) {
+    Start-Sleep -seconds 20
+  }
+
+  while ($serviceState.Status -ne 'Running') {
+    Start-Service $serviceName
+    Start-Sleep -seconds 5
+    $serviceState.Refresh()
+  }
+
+  Stop-Service $serviceName
+  Stop-Process -Name $serviceName -Force > null
+}
+
+function ConfigureRustdesk([string]$rdServer, [string]$rdKey, [bool]$enableAudio, [string]$serviceName) {
+  $ipAddress = (Get-NetIPConfiguration | Where-Object {$_.IPv4DefaultGateway -ne $null -and $_.NetAdapter.status -ne "Disconnected"}).IPv4Address.IPAddress
+
+  # RustDesk2.toml
+  $rd2Toml = @"
+rendezvous_server = '$rdServer'
+nat_type = 2
+serial = 0
+
+[options]
+local-ip-addr = '$ipAddress'
+"@
+
+  if ($rdServer -ne "rs-ny.rustdesk.com") {
+    $rd2Toml += "`ncustom-rendezvous-server = '$rdServer'"
+    $rd2Toml += "`nrelay-server = '$rdServer'"
+    $rd2Toml += "`napi-server = 'https://$rdServer'"
+    if ($rdKey) {
+      $rd2Toml += "`nkey = '$rdKey'"
+    }
+  }
+
+  if (!($enableAudio)) {
+    $rd2Toml += "`nenable-audio = 'N'"
+  }
+
+  if (!(Test-Path $env:AppData\RustDesk\config\RustDesk2.toml)) {
+    New-Item $env:AppData\RustDesk\config\RustDesk2.toml > null
+  }
+  Set-Content $env:AppData\RustDesk\config\RustDesk2.toml $rd2Toml > null
+
+  if (!(Test-Path $env:WinDir\ServiceProfiles\LocalService\AppData\Roaming\RustDesk\config\RustDesk2.toml)) {
+    New-Item $env:WinDir\ServiceProfiles\LocalService\AppData\Roaming\RustDesk\config\RustDesk2.toml > null
+  }
+  Set-Content $env:WinDir\ServiceProfiles\LocalService\AppData\Roaming\RustDesk\config\RustDesk2.toml $rd2Toml > null
+}
+
+function SetRustdeskPW([int]$pwLength) {
+  $rustdeskPW = (-join ((65..90) + (97..122) | Get-Random -Count $pwLength | % {[char]$_}))
+  $throwAway = ExecuteCommand -commandPath "$env:ProgramFiles\RustDesk\RustDesk.exe" -commandArguments "--password $rustdeskPW"
+
+  #Workaround: Copy permanent password settings from:
+  #    $env:Appdata\RustDesk\config\RustDesk.toml 
+  #  to:
+  #    $env:WinDir\ServiceProfiles\LocalService\AppData\Roaming\RustDesk\config\RustDesk\RustDesk.toml
+  if ("$env:Appdata" -ne "$env:WinDir\ServiceProfiles\LocalService\AppData\Roaming") {
+    Copy-Item -Path "$env:Appdata\RustDesk\config\RustDesk.toml" -Destination "$env:WinDir\ServiceProfiles\LocalService\AppData\Roaming\RustDesk\config\RustDesk.toml" -Force
+  }
+
+  if ($throwAway.ExitCode -eq 0) {
+    return $rustdeskPW
+  } else {
+    return 255
+  }
+}
+
+function GetRustdeskID {
+  $rustdeskID = ExecuteCommand -commandPath "$env:ProgramFiles\RustDesk\rustdesk.exe" -commandArguments " --get-id"
+
+  if ($rustdeskID.ExitCode -eq 0) {
+    return $rustdeskID.stdout.Trim()
+  } else {
+    return 255
+  }
+}
+
+function OutputIDAndPW([string]$rustdeskID, [string]$rustdeskPW) {
+  Write-Output("######################################################")
+  Write-Output("#                                                    #")
+  Write-Output("# CONNECTION PARAMETERS:                             #")
+  Write-Output("#                                                    #")
+  Write-Output("######################################################")
+  Write-Output("")
+  Write-Output("  RustDesk-ID:       $rustdeskID")
+  Write-Output("  RustDesk-Password: $rustdeskPW")
+  Write-Output("")
+}
+
+function WriteRustdeskCredsToNextcloudPasswords([string]ncBaseUrl, [string]$ncUsername, [string]$ncToken, [string]$ncFolder, [string]$rustdeskID, [string]$rustdeskPW) {
+  #
+  # API documentation can be found here https://git.mdns.eu/nextcloud/passwords/-/wikis/Developers/Index
+  #
+  $computerName = $env:ComputerName.ToUpper()
+  $pair = "$($ncUsername):$($ncToken)"
+  $encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
+  $basicAuthValue = "Basic $encodedCreds"
+  $Headers = @{"Authorization" = "$basicAuthValue"}
+
+  $openNCPwSession = Invoke-WebRequest -Uri "$ncBaseUrl/api/1.0/session/open" -Headers $Headers -UseBasicParsing -Method Post -SessionVariable Cookie
+  $listNCPasswords = Invoke-WebRequest -Uri "$ncBaseUrl/api/1.0/password/list" -Headers $Headers -UseBasicParsing -WebSession $Cookie
+  $pwEntries = ConvertFrom-Json($listNCPasswords.Content)
+
+  # Find record by label
+  foreach ($pwEntry in $pwEntries) {
+    if ($pwEntry.folder -eq "$ncFolder") {
+      if ($pwEntry.label -eq $computerName) {
+          $pwID = $pwEntry.id
+      }
+    }
+  }
+
+  $rustdeskPWStream = [IO.MemoryStream]::new([byte[]][char[]]$rustdeskPW)
+  $rustdeskPWHash = Get-FileHash -InputStream $rustdeskPWStream -Algorithm SHA1
+  $rustdeskHash = $rustdeskPWHash.Hash.ToLower()
+
+  if ($pwID -eq $null) {
+    #create entry
+    $JSON = @{
+      "password" = "$rustdeskPW";
+      "label" = "$computerName";
+      "username" = "$rustdeskID";
+      "folder" = "$ncFolder";
+      "hash" = "$rustdeskHash";
+    } | ConvertTo-Json
+    $createNCPassword = Invoke-WebRequest -Uri "$ncBaseUrl/api/1.0/password/create" -Headers $Headers -UseBasicParsing -WebSession $Cookie -Body $JSON -ContentType application/json -Method Post
+  } else {
+    #update entry
+    $JSON = @{
+      "id" = "$pwID";
+      "password" = "$rustdeskPW";
+      "label" = "$computerName";
+      "username" = "$rustdeskID";
+      "folder" = "$ncFolder";
+      "hash" = "$rustdeskHash";
+    } | ConvertTo-Json
+    $updateNCPassword = Invoke-WebRequest -Uri "$ncBaseUrl/api/1.0/password/update" -Headers $Headers -UseBasicParsing -WebSession $Cookie -Body $JSON -ContentType application/json -Method Patch
+  }
+}
+
+# Run all functions
+$rdUpstreamVersion = PreqRustdeskUpstreamVersion -rustdeskURL $rustdeskURL
+$rdInstalledVersion = PreqRustdeskInstalledVersion -rustdeskReg $rustdeskReg
+$serviceName = 'Rustdesk'
+
+Prerequisites -VersionInstalled $rdInstalledVersion -VersionUpstream $rdUpstreamVersion
+DownloadRustdesk -version $rdUpstreamVersion
+InstallRustdesk
+StopRustdesk -serviceName $serviceName
+ConfigureRustdesk -rdServer $rdServer -rdKey $rdKey -enableAudio $enableAudio -serviceName $serviceName
+$rustdeskPW = SetRustdeskPW -pwLength $pwLength 
+$rustdeskID = GetRustdeskID
+OutputIDAndPW -rustdeskID $rustdeskID -rustdeskPW $rustdeskPW
+StartRustdesk -serviceName $serviceName
+
+if ($toNextcloudPassword) {
+  $ncParams = @{
+    "ncBaseUrl" = "$ncBaseUrl";
+    "ncUsername" = "$ncUsername";
+    "ncToken" = "$ncToken";
+    "ncFolder" = "$ncFolder";
+    "rustdeskID" = "$rustdeskID";
+    "rustdeskPW" = "$rustdeskPW"
+  }
+  WriteRustdeskCredsToNextcloudPasswords $ncParams
+}
